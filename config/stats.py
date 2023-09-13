e9802b07a49b29f4b65e89c5ddf4b2c0da938523
zakariaYeo <59830605+Junny-cloud@users.noreply.github.com>
2023-09-13 18:16:51 +0000

    update mercredi

--- config/stats.py
+++ config/stats.py
@@ -55,7 +55,7 @@ def vente_data():
         total = Commandes.objects.filter(Q(date_registry__year=year, date_registry__month=m)).aggregate(somme_prix=Coalesce(Sum('total_amount'), 0))['somme_prix']
         data_month_all[x_month][1]=total
         data_month.append(int(total))
-    return data_month_all
+    return data_month
     
 
 
@@ -73,7 +73,7 @@ year_commandes_total = Commandes.objects.filter(Q(date_registry__year=year, stat
 montant_vente_per_month = vente_data()
 
 produits_count_commandes_data = []
-tx =[obj for obj in ProduitsCommandes.objects.values('product__name', 'product__price','product__id').annotate(nombre =Sum('quantity')).order_by('product__id')]
+tx =[obj for obj in ProduitsCommandes.objects.values('product__name', 'product__price','product__id').annotate(nombre =Sum('quantity')).order_by('-nombre')]
 for obj in tx:
     img = Image.objects.filter(product__id=obj['product__id']).first()
     img_url = img.image.name
@@ -87,7 +87,8 @@ commentaires_count = Commentaires.objects.all().count()
 today_commentaires_count = Commentaires.objects.filter(date_registry=date.today()).count()
 admin_count = CustomUser.objects.filter(is_staff=True).count() 
 new_admin_count = CustomUser.objects.filter(Q(date_joined__year=year, date_joined__month=month, is_superuser=True)).count() 
-
+liste_comments = [obj for obj in Commentaires.objects.values('contenu', 'client__first_name','note','client__last_name','date_registry')]
+list_commandes = [obj for obj in Commandes.objects.values('reference', 'user__first_name','total_amount','user__last_name','date_registry', 'user__email', 'user__telephone', 'etat_commande')]
 # Votre JSON en tant que variable
 stats_json = {
     "today_commandes_count":today_commandes_count,
@@ -113,6 +114,8 @@ stats_json = {
     "today_commentaires_count":today_commentaires_count,
     "admin_count": admin_count,
     "new_admin_count":new_admin_count,
+    "liste_comments":liste_comments,
+    "list_commandes":list_commandes,
 }
 
 class ProduitsCountCommandesType(graphene.ObjectType):
@@ -121,6 +124,13 @@ class ProduitsCountCommandesType(graphene.ObjectType):
     product__id = graphene.String()
     nombre = graphene.Int()
     image = graphene.String()
+    
+class CommentsType(graphene.ObjectType):
+    contenu =graphene.String()
+    client__firstname =graphene.String()
+    client__lastname = graphene.String()
+    date_registry = graphene.DateTime()
+    note = graphene.Int()
 # Définissez un type GraphQL pour votre JSON
 class StatsType(graphene.ObjectType):
     
@@ -142,4 +152,5 @@ class StatsType(graphene.ObjectType):
     today_commentaires_count=graphene.Int()
     admin_count =graphene.Int()
     new_admin_count=graphene.Int()
+    liste_comments=graphene.List(CommentsType) 
     
